58743f30dd0cd8689e0c1c793707759c0588197c
Shiro Han <shan99business@gmail.com>
2020-05-21 20:02:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   output = []
   count = 0
-  while array[count] do
+  while array[count] {
     squared = array[count] ** 2
     output << squared
     count += 1
-  end
+  }
   output
 end
